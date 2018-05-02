8c88c043b2633fab413d41591badcfa16c7a8b37
miyakz1192 <miyakz1192@gmail.com>
2018-05-02 22:50:40 +0900

    ctags added

---
+++ build_ctags_env.sh
@@ -0,0 +1,26 @@
+echo "building ctags env(TypeScript)"
+
+ctags --version | grep -i universal > /dev/null
+if [ $? -ne 0 ]; then
+  echo "universal ctags is not found. install it?[y/n]"
+  read input
+  if [ "${input}" != "y" ]; then
+    echo "cancel"
+    exit 1
+  fi
+else
+  echo "universal ctags is already installed in this environment."
+  exit 0
+fi
+
+echo "install universal ctag"
+
+git clone https://github.com/universal-ctags/ctags.git
+cd ctags
+./autogen.sh
+./configure
+make 
+make install
+
+
+
